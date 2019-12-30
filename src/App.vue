949708566a27efd245a850ac44830d75c6ecd2de
Keith Ong <keith.oys@gmail.com>
2019-12-30 10:54:53 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,211 @@
+<template>
+  <div class="flexible-content">
+    <!--Navbar-->
+    <mdb-navbar class="flexible-navbar white" light position="top" scrolling>
+      <mdb-navbar-brand href="#" target="_blank">Loan App</mdb-navbar-brand>
+      <mdb-navbar-toggler>
+        <mdb-navbar-nav left> </mdb-navbar-nav>
+        <mdb-navbar-nav right>
+          <mdb-nav-item href="#!" waves-fixed>
+            <mdb-icon fab class="text-black" icon="facebook-square" />
+          </mdb-nav-item>
+          <mdb-nav-item href="#!" waves-fixed>
+            <mdb-icon fab icon="twitter" />
+          </mdb-nav-item>
+          <mdb-nav-item
+            href="#"
+            waves-fixed
+            class="border border-light rounded"
+            target="_blank"
+          >
+            <i class="fas fa-sign-out-alt"></i>Log Out
+          </mdb-nav-item>
+        </mdb-navbar-nav>
+      </mdb-navbar-toggler>
+    </mdb-navbar>
+    <!--/.Navbar-->
+    <!-- Sidebar -->
+    <div class="sidebar-fixed position-fixed">
+      <a class="logo-wrapper">
+        <img alt class="img-fluid" src="./assets/logo-mdb-vue-small.png" />
+      </a>
+      <mdb-list-group class="list-group-flush">
+        <router-link to="/dashboard" @click.native="activeItem = 1">
+          <mdb-list-group-item
+            :action="true"
+            :class="activeItem === 1 && 'active'"
+          >
+            <mdb-icon icon="table" class="mr-3" />Loan List
+          </mdb-list-group-item>
+        </router-link>
+        <router-link to="/loan-application" @click.native="activeItem = 2">
+          <mdb-list-group-item
+            :action="true"
+            :class="activeItem === 2 && 'active'"
+          >
+            <mdb-icon icon="edit" class="mr-3" />Loan Application
+          </mdb-list-group-item>
+        </router-link>
+        <router-link to="/tables" @click.native="activeItem = 3">
+          <mdb-list-group-item
+            :action="true"
+            :class="activeItem === 3 && 'active'"
+          >
+            <mdb-icon icon="diagnoses" class="mr-3" />Server Diagnostics
+          </mdb-list-group-item>
+        </router-link>
+      </mdb-list-group>
+    </div>
+    <!-- /Sidebar  -->
+    <main>
+      <div class="mt-5 p-5">
+        <router-view></router-view>
+      </div>
+      <!-- Footer -->
+      <ftr color="primary-color-dark" class="text-center font-small darken-2">
+        <hr class="my4" />
+        <div class="pb-4">
+          <a href="#">
+            <mdb-icon fab icon="facebook-square" class="mr-3" />
+          </a>
+          <a href="#">
+            <mdb-icon fab icon="twitter" class="mr-3" />
+          </a>
+          <a href="#">
+            <mdb-icon fab icon="youtube" class="mr-3" />
+          </a>
+          <a href="#">
+            <mdb-icon fab icon="google-plus" class="mr-3" />
+          </a>
+          <a href="#">
+            <mdb-icon fab icon="dribbble" class="mr-3" />
+          </a>
+          <a href="#">
+            <mdb-icon fab icon="pinterest" class="mr-3" />
+          </a>
+          <a href="#">
+            <mdb-icon fab icon="github" class="mr-3" />
+          </a>
+          <a href="#">
+            <mdb-icon fab icon="codepen" class="mr-3" />
+          </a>
+        </div>
+        <p class="footer-copyright mb-0 py-3 text-center">
+          &copy; {{ new Date().getFullYear() }} Copyright Keith Ong
+        </p>
+      </ftr>
+    </main>
+  </div>
+</template>
+
+<script>
+import {
+  mdbContainer,
+  mdbNavbar,
+  mdbNavbarBrand,
+  mdbNavItem,
+  mdbNavbarNav,
+  mdbNavbarToggler,
+  mdbBtn,
+  mdbIcon,
+  mdbListGroup,
+  mdbListGroupItem,
+  mdbCardBody,
+  mdbFooter,
+  waves
+} from 'mdbvue'
+import Dashboard from './components/Dashboard'
+import LoanApplication from './components/LoanApplication'
+
+export default {
+  name: 'AdminTemplate',
+  components: {
+    mdbContainer,
+    mdbNavbar,
+    mdbNavbarBrand,
+    mdbNavItem,
+    mdbNavbarNav,
+    mdbNavbarToggler,
+    mdbBtn,
+    mdbListGroup,
+    mdbListGroupItem,
+    mdbIcon,
+    mdbCardBody,
+    ftr: mdbFooter,
+    Dashboard,
+    LoanApplication
+  },
+  data() {
+    return {
+      activeItem: 1
+    }
+  },
+  beforeMount() {
+    this.activeItem = this.$route.matched[0].props.default.page
+  },
+  mixins: [waves]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+main {
+  background-color: #ededee;
+}
+
+.flexible-content {
+  transition: padding-left 0.3s;
+  padding-left: 270px;
+}
+
+.flexible-navbar {
+  transition: padding-left 0.5s;
+  padding-left: 270px;
+}
+
+.sidebar-fixed {
+  left: 0;
+  top: 0;
+  height: 100vh;
+  width: 270px;
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  z-index: 1050;
+  background-color: #fff;
+  padding: 1.5rem;
+  padding-top: 0;
+}
+
+.sidebar-fixed .logo-wrapper img {
+  width: 100%;
+  padding: 2.5rem;
+}
+
+.sidebar-fixed .list-group-item {
+  display: block !important;
+  transition: background-color 0.3s;
+}
+
+.sidebar-fixed .list-group .active {
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  border-radius: 5px;
+}
+
+@media (max-width: 1199.98px) {
+  .sidebar-fixed {
+    display: none;
+  }
+  .flexible-content {
+    padding-left: 0;
+  }
+  .flexible-navbar {
+    padding-left: 10px;
+  }
+}
+</style>
+<style>
+.navbar-light .navbar-brand {
+  margin-left: 15px;
+  color: #2196f3 !important;
+  font-weight: bolder;
+}
+</style>
---
+++ src/components/LoanApplication.vue
@@ -0,0 +1,318 @@
+<template>
+  <section>
+    <mdb-row>
+      <mdb-col md="12">
+        <mdb-card cascade narrow class="mt-5 mb-5">
+          <mdb-view class="gradient-card-header blue darken-2">
+            <h4 class="h4-responsive text-white">Loan Application</h4>
+          </mdb-view>
+          <mdb-container class="mb-5">
+            <!-- Custom styles -->
+            <h2 class="my-5"></h2>
+            <form class="needs-validation" novalidate @submit="submitForm">
+              <!-- Product Type -->
+              <div class="form-row">
+                <div class="col-md-12 mb-4">
+                  <label for="cbType" style="font-weight: bold"
+                    >Product Type</label
+                  >
+                  <br />
+                  <div
+                    class="custom-control custom-radio custom-control-inline"
+                  >
+                    <input
+                      type="radio"
+                      class="custom-control-input"
+                      id="cbTypePO"
+                      name="cbType"
+                      v-model="type"
+                      v-bind:value="'typePO'"
+                      checked
+                      required
+                    />
+                    <label class="custom-control-label" for="cbTypePO"
+                      >Purchase Order</label
+                    >
+                  </div>
+
+                  <div
+                    class="custom-control custom-radio custom-control-inline"
+                  >
+                    <input
+                      type="radio"
+                      class="custom-control-input"
+                      id="cbTypeIF"
+                      name="cbType"
+                      v-model="type"
+                      v-bind:value="'typeIF'"
+                      required
+                    />
+                    <label class="custom-control-label" for="cbTypeIF"
+                      >Invoice Financing</label
+                    >
+                  </div>
+                  <div class="valid-feedback">Looks good!</div>
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="col-md-6 mb-4">
+                  <!-- Loan Amount -->
+                  <label for="tbAmount" style="font-weight: bold"
+                    >Loan Amount</label
+                  >
+                  <br />
+                  <input
+                    type="number"
+                    class="form-control"
+                    id="tbAmount"
+                    placeholder="0"
+                    min="0"
+                    v-model="amount"
+                    outline
+                    required
+                  />
+                  <div class="valid-feedback">Looks good!</div>
+                  <div class="invalid-feedback">Amount is not valid</div>
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="col-md-6 mb-4">
+                  <!-- Interest Rate -->
+                  <label for="cbInterest" style="font-weight: bold" class="mb-2"
+                    >Interest Rate (based on product type chosen)</label
+                  >
+                  <br />
+                  <div
+                    class="custom-control custom-radio custom-control-inline"
+                  >
+                    <input
+                      type="radio"
+                      class="custom-control-input"
+                      id="cbInterest15"
+                      name="cbInterest"
+                      v-bind:checked="type == 'typePO'"
+                      disabled
+                    />
+                    <label class="custom-control-label" for="cbInterest15"
+                      >1.5%</label
+                    >
+                  </div>
+
+                  <div
+                    class="custom-control custom-radio custom-control-inline"
+                  >
+                    <input
+                      type="radio"
+                      class="custom-control-input"
+                      id="cbInterest18"
+                      name="cbInterest"
+                      v-bind:checked="type == 'typeIF'"
+                      disabled
+                    />
+
+                    <label class="custom-control-label" for="cbInterest18"
+                      >1.8%</label
+                    >
+                  </div>
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="col-md-6 mb-4">
+                  <!-- Terms -->
+                  <label for="cbTerms" style="font-weight: bold" class="mb-2"
+                    >Terms (months)</label
+                  >
+                  <br />
+                  <div
+                    class="custom-control custom-radio custom-control-inline"
+                  >
+                    <input
+                      type="radio"
+                      class="custom-control-input"
+                      id="cbTerms3"
+                      name="cbTerms"
+                      checked
+                      required
+                    />
+                    <label class="custom-control-label" for="cbTerms3">3</label>
+                  </div>
+
+                  <div
+                    class="custom-control custom-radio custom-control-inline"
+                  >
+                    <input
+                      type="radio"
+                      class="custom-control-input"
+                      id="cbTerms6"
+                      name="cbTerms"
+                      required
+                    />
+                    <label class="custom-control-label" for="cbTerms6">6</label>
+                  </div>
+
+                  <div
+                    class="custom-control custom-radio custom-control-inline"
+                  >
+                    <input
+                      type="radio"
+                      class="custom-control-input"
+                      id="cbTerms12"
+                      name="cbTerms"
+                      required
+                    />
+                    <label class="custom-control-label" for="cbTerms12"
+                      >12</label
+                    >
+                  </div>
+
+                  <div
+                    class="custom-control custom-radio custom-control-inline"
+                  >
+                    <input
+                      type="radio"
+                      class="custom-control-input"
+                      id="cbTerms24"
+                      name="cbTerms"
+                      required
+                    />
+                    <label class="custom-control-label" for="cbTerms24"
+                      >24</label
+                    >
+                  </div>
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="col-md-6 mb-4">
+                  <!-- Documents -->
+                  <label style="font-weight: bold" class="mb-2"
+                    >Documents</label
+                  >
+                  <br />
+                  <div class="custom-file">
+                    <input
+                      type="file"
+                      class="custom-file-input"
+                      id="upload"
+                      required
+                    />
+                    <label class="custom-file-label" for="upload"
+                      >Choose file...</label
+                    >
+                    <div class="invalid-feedback">A document is required</div>
+                  </div>
+                </div>
+              </div>
+
+              <mdb-btn color="primary" type="submit">Submit form</mdb-btn>
+            </form>
+          </mdb-container>
+        </mdb-card>
+      </mdb-col>
+    </mdb-row>
+  </section>
+</template>
+
+<script>
+import {
+  mdbRow,
+  mdbCol,
+  mdbCard,
+  mdbCardBody,
+  mdbView,
+  mdbMask,
+  mdbCardTitle,
+  mdbCardText,
+  mdbCardFooter,
+  mdbContainer,
+  mdbIcon,
+  mdbBtn,
+  mdbPagination,
+  mdbPageNav,
+  mdbPageItem
+} from 'mdbvue'
+import Vue from 'vue'
+import $ from 'jquery'
+import loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+
+Vue.use(loading)
+
+export default {
+  name: 'LoanApplication',
+  components: {
+    mdbRow,
+    mdbCol,
+    mdbCard,
+    mdbCardBody,
+    mdbView,
+    mdbMask,
+    mdbCardTitle,
+    mdbCardText,
+    mdbCardFooter,
+    mdbContainer,
+    mdbIcon,
+    mdbBtn,
+    mdbPagination,
+    mdbPageNav,
+    mdbPageItem,
+    loading
+  },
+  data() {
+    return {
+      type: 'typePO',
+      amount: 0,
+      interest: 1.5,
+      terms: 3,
+      documents: '',
+      fullPage: false
+    }
+  },
+  mounted() {
+    $('input[type="file"]').change(function(e) {
+      var fileName = e.target.files[0].name
+      $('.custom-file-label').html(fileName)
+      this.documents = fileName
+    })
+  },
+  methods: {
+    submitForm(event) {
+      event.preventDefault()
+      event.target.classList.add('was-validated')
+      this.displayLoader()
+      // AXIOS -> MySQL
+    },
+    displayLoader() {
+      let loader = this.$loading.show({
+        color: '#4285F4',
+        backgroundColor: 'bdbdbd',
+        loader: 'dots',
+        container: this.fullPage ? null : this.$refs.formContainer,
+        canCancel: true,
+        onCancel: this.onCancel
+      })
+      setTimeout(() => {
+        loader.hide()
+      }, 5000)
+    },
+    onCancel() {
+      console.log('User cancelled the loader.')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card.card-cascade .view.gradient-card-header {
+  padding: 1rem 1rem;
+  text-align: center;
+}
+.card.card-cascade h3,
+.card.card-cascade h4 {
+  margin-bottom: 0;
+}
+</style>
